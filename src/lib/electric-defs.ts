ce58adbbba8fa3f21e026015033f106768de0853
kevindotklein <kevindotklein@gmail.com>
2025-06-11 19:39:57 -0300

    feat: electric and mechanical sales page

---
+++ src/lib/electric-defs.ts
@@ -0,0 +1,731 @@
+export const electricTools = [
+  {
+    title: `04.017.0006`,
+    subtitle: `IHM - COLORIDA - 5,6” - SERIAL`,
+  },
+
+  {
+    title: `04.017.0021`,
+    subtitle: `IHM - COLORIDA - 5,6" – ETHERNET`,
+  },
+
+  {
+    title: `04.017.0003`,
+    subtitle: `IHM – COLORIDA – 3,5” – SERIAL`,
+  },
+
+  {
+    title: `04.017.0210`,
+    subtitle: `CLP – CP2E – 30 PONTOS – 18E/12S - PNP`,
+  },
+
+  {
+    title: `- - -`,
+    subtitle: `CLP - CP2E - 40 PONTOS - 24E/16S - PNP`,
+  },
+
+  {
+    title: `- - -`,
+    subtitle: `MÓDULO EXPANSÃO – CP2E – 40 PONTOS – 24E/16S – PNP`,
+  },
+
+  {
+    title: `04.017.0208`,
+    subtitle: `CLP - NX1P - 24 PONTOS – 14E/10S – PNP – CONTROLE EIXOS`,
+  },
+
+  {
+    title: `04.017.0022`,
+    subtitle: `CLP - NX1P - 40 PONTOS – 24E/16S – PNP - CONTROLE EIXOS`,
+  },
+
+  {
+    title: `04.017.0180`,
+    subtitle: `CLP - NX1P - 40 PONTOS – 24E/16S – PNP - CONTROLE EIXOS`,
+  },
+
+  {
+    title: `04.017.0048`,
+    subtitle: `MÓDULO EXPANSÃO - NX - ALIMENTAÇÃO`,
+  },
+
+  {
+    title: `04.017.0055`,
+    subtitle: `MÓDULO EXPANSÃO - NX - ENCODER - PNP - ATE 500KHZ`,
+  },
+
+  {
+    title: `04.017.0031`,
+    subtitle: `MÓDULO EXPANSÃO - NX - 16 PONTOS ENTRADAS - PNP`,
+  },
+
+  {
+    title: `04.017.0023`,
+    subtitle: `MÓDULO EXPANSÃO - NX - 16 PONTOS SAÍDAS - PNP`,
+  },
+
+  {
+    title: `04.017.0209`,
+    subtitle: `MÓDULO DE COMUNICACAO SERIAL - NX - 485/422`,
+  },
+
+  {
+    title: `04.009.0004`,
+    subtitle: `RELE DE SEGURANÇA – 24VDC – 1 SAIDA AUXILIAR`,
+  },
+
+  {
+    title: `04.017.0207`,
+    subtitle: `CONTROLADOR DE TEMPERATURA - P/TRILHO - 24VDC - RS-485`,
+  },
+
+  {
+    title: `04.017.0039`,
+    subtitle: `CONTROLADOR - TEMPER. - DIGITAL - 48X48 - 24VDC - OMRON`,
+  },
+
+  {
+    title: `04.006.0001`,
+    subtitle: `INVERSOR VETORIAL - TRIF. - 220V – 0,5CV – 0,4KW`,
+  },
+
+  {
+    title: `04.006.0002`,
+    subtitle: `INVERSOR VETORIAL - TRIF. - 220V – 1,0CV – 0,75KW`,
+  },
+
+  {
+    title: `04.006.0013`,
+    subtitle: `INVERSOR VETORIAL - TRIF. - 220V – 2,0CV – 1,5KW`,
+  },
+
+  {
+    title: `04.006.0014`,
+    subtitle: `INVERSOR VETORIAL - TRIF. - 220V – 3,0CV – 2,2KW`,
+  },
+
+  {
+    title: `04.006.0006`,
+    subtitle: `INVERSOR VETORIAL - TRIF. - 220V – 5,0CV – 3,7KW`,
+  },
+
+  {
+    title: `04.006.0017`,
+    subtitle: `INVERSOR VETORIAL - TRIF. - 220V – 7,5CV – 5,5KW`,
+  },
+
+  {
+    title: `04.006.0020`,
+    subtitle: `INVERSOR VETORIAL - TRIF. - 220V – 10CV – 7,5KW`,
+  },
+
+  {
+    title: `- - -`,
+    subtitle: `INVERSOR VETORIAL TRIF. - 380V – 0,5CV – 0,4KW`,
+  },
+
+  {
+    title: `04.006.0019 / 04.006.0003`,
+    subtitle: `INVERSOR VETORIAL TRIF. - 380V – 1CV – 0,75KW`,
+  },
+
+  {
+    title: `04.006.0016`,
+    subtitle: `INVERSOR VETORIAL TRIF. - 380V – 2CV – 1,5KW`,
+  },
+
+  {
+    title: `04.006.0004`,
+    subtitle: `INVERSOR VETORIAL TRIF. - 380V – 3CV – 2,2KW`,
+  },
+
+  {
+    title: `04.006.0008`,
+    subtitle: `INVERSOR VETORIAL TRIF. - 380V – 5CV – 4,0KW`,
+  },
+
+  {
+    title: `04.006.0018`,
+    subtitle: `INVERSOR VETORIAL TRIF. - 380V – 7,5CV – 5,5KW`,
+  },
+
+  {
+    title: `- - -`,
+    subtitle: `SERVO DRIVE G5 - 200VAC - 750W - R88D-KT08H`,
+  },
+
+  {
+    title: `- - -`,
+    subtitle: `SERVO MOTOR – 750W - R88M -K75030H-S2`,
+  },
+
+  {
+    title: `- - -`,
+    subtitle: `CABO R88A-CAKA005S *(CABO DE POTÊNCIA) CABO SERVO - DRIVE G5 R88D-KT08H / SERVO - MOTOR R88MK75030H-S2`,
+  },
+
+  {
+    title: `- - -`,
+    subtitle: `CABO R88A-CRKA005C - *(CABO RESOLVER/ENCODER) - CABO SERVO DRIVE G5 R88D - KT08H / SERVO MOTOR - 88MK75030H-S2`,
+  },
+
+  {
+    title: `- - -`,
+    subtitle: `CABO R88A-CPG001S *(CABO SINAIS DE COMANDO) - CABO SERVO DRIVE G5 R88D-KT08H / CLP - OMRON - CP1E/CP2E/CP1H`,
+  },
+
+  {
+    title: `- - -`,
+    subtitle: `SERVO DRIVER – 200VAC – 400W - R88D-1SN04H-ECT`,
+  },
+
+  {
+    title: `- - -`,
+    subtitle: `SERVO MOTOR SERIE 1S – 400W - R88M-1M40030T-S2`,
+  },
+
+  {
+    title: `- - -`,
+    subtitle: `CABO R88A-CA1A005SF *(CABO DE POTÊNCIA)`,
+  },
+
+  {
+    title: `04.014.0039`,
+    subtitle: `FONTE DE ALIMENTAÇÃO 24VDC/2,5 A – TRILHO DIN`,
+  },
+
+  {
+    title: `04.014.0007`,
+    subtitle: `FONTE DE ALIMENTAÇÃO 24VDC/5,0 A – TRILHO DIN`,
+  },
+
+  {
+    title: `04.014.0034`,
+    subtitle: `FONTE DE ALIMENTAÇÃO 24VDC/10 A – TRILHO DIN`,
+  },
+
+  {
+    title: `- - -`,
+    subtitle: `FONTE DE ALIMENTAÇÃO 24VDC/20 A – TRILHO DIN`,
+  },
+
+  {
+    title: `- - -`,
+    subtitle: `FONTE DE ALIMENTAÇÃO 48VDC/13 A – 600W`,
+  },
+
+  {
+    title: `04.014.0060`,
+    subtitle: `FONTE DE ALIMENTAÇÃO 12V/1,25 A – 15W`,
+  },
+
+  {
+    title: `04.009.0027`,
+    subtitle: `RELE - ELETROMEC. - MY4N – 24VDC – 4 CONTATOS NA/NF – COM LED *(MY4N-DC24 + PYFZ-14-E)`,
+  },
+
+  {
+    title: `04.009.0006`,
+    subtitle: `RELE - ELETROMEC. - MY4N - 20/240VAC – 4 CONTATOS NA/NF`,
+  },
+
+  {
+    title: `– COM LED *(MY4N-CR + PYFZ-14-E)`,
+    subtitle: ``,
+  },
+
+  {
+    title: `04.009.0104`,
+    subtitle: `BASE - P/ RELE - MY4N`,
+  },
+
+  {
+    title: `- - -`,
+    subtitle: `RELE – ELETROMEC. G2RV – 24VDC – 1 CONTATO NA/NF – 6,2MM – BORNE MOLA`,
+  },
+
+  {
+    title: `- - -`,
+    subtitle: `RELE – ELETROMEC. G2RV – 24VDC – 1 CONTATO NA/NF – 6,2MM – BORNE PARAFUSO`,
+  },
+
+  {
+    title: `04.001.0031`,
+    subtitle: `CHAVE DE SEGURANÇA MAGNÉTICA – 2NF/1NA – 5M`,
+  },
+
+  {
+    title: `04.007.0030`,
+    subtitle: `TRAVA DE SEGURANÇA D4NS4CF – 2NF/1NA *(D4NS-4CF + D4DS-K2)`,
+  },
+
+  {
+    title: `04.007.0012`,
+    subtitle: `ATUADOR PARA TRAVA DE SEGURANÇA D4NS4CF MONT. VERT. OMRON - D4DS-K2`,
+  },
+
+  {
+    title: `- - -`,
+    subtitle: `AMPLIFICADOR *KIT FIBRA(AMPLIFICADOR + FIBRA + CABO) DATALOGIC - S7-6-E-P`,
+  },
+
+  {
+    title: `- - -`,
+    subtitle: `FIBRA *KIT FIBRA(AMPLIFICADOR + FIBRA + CABO) HIDROAIR - OF-42-ST-20 - PARTNUMBER - 95A201340`,
+  },
+
+  {
+    title: `04.001.0029`,
+    subtitle: `SENSOR FOTOELET. OMRON – 8MM – 4 PINOS – PNP – NA+NF – 12/24VDC *KIT(E3Z-LS86 + XS3F-M8PVC4A5M ou XS3F - PVC4S5M ou XS3F-M8PVC4A2M)`,
+  },
+
+  {
+    title: `04.001.0053`,
+    subtitle: `CABO P/ SENSOR OMRON – 8MM - 4 PINOS – RADIAL - 5M`,
+  },
+
+  {
+    title: `04.001.0059`,
+    subtitle: `CABO P/ SENSOR OMRON – 8MM - 4 PINOS – RADIAL - 2M`,
+  },
+
+  {
+    title: `04.001.0047`,
+    subtitle: `CABO P/ SENSOR OMRON – 8MM - 4 PINOS – AXIAL - 5M`,
+  },
+
+  {
+    title: `04.001.0007`,
+    subtitle: `SENSOR FOTOELET. TUBULAR M18 - DATALOGIC – 12MM – 4 - PINOS – PNP - *KIT(S15-PA-5-M01-PK*B + SMC-SAC-4P-5-PVCM12FR)`,
+  },
+
+  {
+    title: `04.001.0008`,
+    subtitle: `CABO P/ SENSOR DATALOGIC – 12MM – 4 PINOS – RADIAL – 5M *Temperatura de funcionamento: -25°C ~ 90°C`,
+  },
+
+  {
+    title: `04.001.0030`,
+    subtitle: `CABO P/ SENSOR DATALOGIC – 12MM - 4 PINOS – RADIAL – 5M *Temperatura de funcionamento: até 80°C (Inutilizado)`,
+  },
+
+  {
+    title: `04.001.0005`,
+    subtitle: `SENSOR INDUTIVO DATALOGIC – M8 – 3 PINOS - PNP – NO *KIT(IS-08-A1-S1 + CS-B1-01-G-05 ou CS-B2-01-G-05)`,
+  },
+
+  {
+    title: `04.001.0054`,
+    subtitle: `SENSOR INDUTIVO DATALOGIC – M12 – 3 PINOS - PNP – NO *KIT(IS-08-A1-S2 + CS-A1-01-G-05 ou CS-A2-01-G-05)`,
+  },
+
+  {
+    title: `- - -`,
+    subtitle: `CABO P/ SENSOR DATALOGIC – 8MM - 3 PINOS – AXIAL – 5M`,
+  },
+
+  {
+    title: `04.001.0006`,
+    subtitle: `CABO P/ SENSOR DATALOGIC – 8MM - 3 PINOS – RADIAL - 5M`,
+  },
+
+  {
+    title: `- - -`,
+    subtitle: `CABO P/ SENSOR DATALOGIC – 12MM – 3 PINOS – AXIAL – 5M`,
+  },
+
+  {
+    title: `- - -`,
+    subtitle: `CABO P/ SENSOR DATALOGIC – 12MM - 3 PINOS – RADIAL - 5M`,
+  },
+
+  {
+    title: `04.001.0010`,
+    subtitle: `SENSOR MAGNÉTICO SMC – 3 FIOS – PNP – 3M *(D-M9BL + BJ3-1 + BJ2-0XX) *KIT(XX = em função do diâmetro do cilindro)`,
+  },
+
+  {
+    title: `04.001.0011`,
+    subtitle: `SUPORTE P/ SENSOR MAGNÉTICO `,
+  },
+
+  {
+    title: `04.001.0018`,
+    subtitle: `ABRAÇADEIRA P/ SENSOR MAGNÉTICO SMC – DM9BL`,
+  },
+
+  {
+    title: `04.002.0004`,
+    subtitle: `ENCODER INCREMENTAL - HOHNER (5810-0332-0360-Z03) CABO DE 6M`,
+  },
+
+  {
+    title: `04.001.0015`,
+    subtitle: `TERMOPAR (FE-CO) TIPO J - 6MT`,
+  },
+
+  {
+    title: `04.014.0030`,
+    subtitle: `TRANSFORMADOR - DE CORRENTE - RENZ (TC15A RH40)`,
+  },
+
+  {
+    title: `04.017.0214`,
+    subtitle: `HORIMETRO – TOTALIZADOR DE IMPULSO/ HORAS - 24 A 240VCA/VCC – DIGIMEC - LTH-S`,
+  },
+
+  {
+    title: `04.017.0188`,
+    subtitle: `VOLTIMETRO + AMPERIMETRO - 0>100V - 10A DUAL DISPLAY - LED - DC`,
+  },
+
+  {
+    title: `- - -`,
+    subtitle: `CAPA METÁLICA 90 GRAUS PARA CONECTOR DB9`,
+  },
+
+  {
+    title: `04.009.0030`,
+    subtitle: `FUSÍVEL DE VIDRO 5X20 - 0,2A (200mA) 250V`,
+  },
+
+  {
+    title: `04.007.0003`,
+    subtitle: `IDENTIFICADOR DE EMERGÊNCIA PARA BOTÃO 22MM - DIAMETRO 60MM - AMARELO`,
+  },
+
+  {
+    title: `04.010.0026`,
+    subtitle: `BORNE CONECTOR PORCELA TRIPOLAR PARA CABO/FIO 10MM C/ FURO`,
+  },
+
+  {
+    title: `- - -`,
+    subtitle: `BORNE – DUPLO – MOLAS – 1,5MM - PHOENIX`,
+  },
+
+  {
+    title: `04.010.0024`,
+    subtitle: `BORNE - DUPLO - MOLAS - 2,5MM – PHOENIX`,
+  },
+
+  {
+    title: `- - -`,
+    subtitle: `BORNE – DUPLO – MOLA – 1,5MM - PHOENIX`,
+  },
+
+  {
+    title: `04.010.0062`,
+    subtitle: `BORNE – SIMPLES – MOLAS – 2,5MM - PHOENIX`,
+  },
+
+  {
+    title: `04.010.0063`,
+    subtitle: `BORNE – SIMPLES – MOLAS – 4,0MM - PHOENIX`,
+  },
+
+  {
+    title: `04.010.0064`,
+    subtitle: `BORNE – SIMPLES – MOLAS – 6,0MM - PHOENIX`,
+  },
+
+  {
+    title: `04.010.0065`,
+    subtitle: `BORNE – SIMPLES – MOLAS – 10MM - PHOENIX`,
+  },
+
+  {
+    title: `04.010.0066`,
+    subtitle: `BORNE – SIMPLES – MOLAS – 16MM - PHOENIX`,
+  },
+
+  {
+    title: `04.010.0067`,
+    subtitle: `BORNE PARA SENSOR – MOLAS – 1,5MM - PHOENIX`,
+  },
+
+  {
+    title: `04.010.0060`,
+    subtitle: `BORNE FUSÍVEIS - PHOENIX`,
+  },
+
+  {
+    title: `04.010.0069`,
+    subtitle: `BORNE TERRA – MOLAS – 2,5MM - PHOENIX`,
+  },
+
+  {
+    title: `04.010.0070`,
+    subtitle: `BORNE TERRA – MOLAS – 4,0MM - PHOENIX`,
+  },
+
+  {
+    title: `04.010.0071`,
+    subtitle: `BORNE TERRA – MOLAS – 10MM - PHOENIX`,
+  },
+
+  {
+    title: `04_010_0068`,
+    subtitle: `JUMPER PARA BORNE SENSOR – VERMELHO - PHOENIX`,
+  },
+
+  {
+    title: `- - -`,
+    subtitle: `JUMPER PARA BORNE SENSOR – AZUL - PHOENIX`,
+  },
+
+  {
+    title: `- - -`,
+    subtitle: `JUMPER PARA BORNE SENSOR – CINZA - PHOENIX`,
+  },
+
+  {
+    title: `04.010.0072`,
+    subtitle: `JUMPER PARA BORNE 2,5MM – VERMELHO - PHOENIX`,
+  },
+
+  {
+    title: `04.010.0073`,
+    subtitle: `JUMPER PARA BORNE 4,0MM – VERMELHO - PHOENIX`,
+  },
+
+  {
+    title: `04.010.0074`,
+    subtitle: `JUMPER PARA BORNE 6,0MM – VERMELHO - PHOENIX`,
+  },
+
+  {
+    title: `04.010.0075`,
+    subtitle: `JUMPER PARA BORNE 10MM – VERMELHO - PHOENIX`,
+  },
+
+  {
+    title: `04.010.0076`,
+    subtitle: `TAMPA P/ BORNE FUSÍVEL - PHOENIX`,
+  },
+
+  {
+    title: `04.010.0077`,
+    subtitle: `TAMPA P/ BORNE SENSOR - PHOENIX`,
+  },
+
+  {
+    title: `04.010.0078`,
+    subtitle: `TAMPA P/ BORNE 10MM - PHOENIX`,
+  },
+
+  {
+    title: `---`,
+    subtitle: `---`,
+  },
+
+  {
+    title: `04.010.0080`,
+    subtitle: `TAMPA P/ BORNE PT 1,5/S - PHOENIX`,
+  },
+
+  {
+    title: `04.010.0081`,
+    subtitle: `TAMPA P/ BORNE 6,0MM - PHOENIX`,
+  },
+
+  {
+    title: `- - -`,
+    subtitle: `CHAVE SECCIONADORA WEG – 25A - COM HASTE E MANOPLA`,
+  },
+
+  {
+    title: `04.007.0069`,
+    subtitle: `CHAVE GERAL - 3P - 32A - C/ MANOPLA - SCHNEIDER - VCF1`,
+  },
+
+  {
+    title: `04_007_0067`,
+    subtitle: `CHAVE GERAL - 3P - 12A - C/ MANOPLA – SCHNEIDER - VCF02`,
+  },
+
+  {
+    title: `04.007.0016`,
+    subtitle: `CHAVE GERAL - SECCIONAD. - 3P - 80A - AM/VM - SCHNEIDER - VCF4`,
+  },
+
+  {
+    title: `04.007.0036`,
+    subtitle: `INTERRUPTOR - GERAL - VARIO - 3P - 175A – SCHNEIDER - VCF6`,
+  },
+
+  {
+    title: `- - -`,
+    subtitle: `CONTATOR DE SEGURANÇA WEG – 9A - 24VDC – 1NA – 1NF`,
+  },
+
+  {
+    title: `- - -`,
+    subtitle: `CABO DE CONTROLE EUROCABOS`,
+  },
+
+  {
+    title: `- - -`,
+    subtitle: `CABO DE DADOS RS-485 EUROCABOS`,
+  },
+
+  {
+    title: `- - -`,
+    subtitle: `VENTILADOR 120X120X38MM -115/230VCA - BUCHA`,
+  },
+
+  {
+    title: `- - -`,
+    subtitle: `PROTETOR PLÁSTICO PARA VENTILADOR 120MM CINZA CLARO - C/ FILTRO`,
+  },
+
+  {
+    title: `- - -`,
+    subtitle: `DISJUNTOR EASY9 1P 6A CURVA C – 3000A EZ9F33106`,
+  },
+
+  {
+    title: `- - -`,
+    subtitle: `DISJUNTOR EASY9 1P 10A CURVA C 3000A EZ9F33110`,
+  },
+
+  {
+    title: `04.008.0001`,
+    subtitle: `DISJUNTOR EASY9 2P 6A CURVA C – 3000A EZ9F33206`,
+  },
+
+  {
+    title: `04.008.0002`,
+    subtitle: `DISJUNTOR EASY9 2P 10A CURVA C 3000A EZ9F33210`,
+  },
+
+  {
+    title: `04.008.0006`,
+    subtitle: `DISJUNTOR EASY9 2P 32A CURVA C 3000A EZ9F33232`,
+  },
+
+  {
+    title: `04.008.0003`,
+    subtitle: `DISJUNTOR EASY9 3P 10A CURVA C 3000A EZ9F33310`,
+  },
+
+  {
+    title: `04.008.0009`,
+    subtitle: `DISJUNTOR EASY9 3P 32A CURVA C 3000A EZ9F33332`,
+  },
+
+  {
+    title: `04.009.0106`,
+    subtitle: `CONTATOR - TRI. - 12A - 24VDC - 1NA+1NF - SCHNEIDER (LC1D12BD)`,
+  },
+
+  {
+    title: `04.009.0107`,
+    subtitle: `CONTATOR - TRI. - 25A - 24VDC - 1NA+1NF - SCHNEIDER (LC1D25BD)`,
+  },
+
+  {
+    title: `04.009.0105`,
+    subtitle: `CONTATOR - TRI. - 32A - 24VDC - 1NA+1NF - SCHNEIDER (LC1D32BD)`,
+  },
+
+  {
+    title: `- - -`,
+    subtitle: `CHAVE SELETORA PLÁSTICA KNOB LONGO - 2 POS. FIXAS 90G - 1NA`,
+  },
+
+  {
+    title: `04_007_0002`,
+    subtitle: `CHAVE SELETORA PLÁSTICA KNOB CURTO - 2 POS. FIXAS 90G - 1NA`,
+  },
+
+  {
+    title: `04.007.0056`,
+    subtitle: `CHAVE SELETORA C/CHAVE 22MM METÁLICO - 2 POS. FIXAS 90G - 1NA`,
+  },
+
+  {
+    title: `- - -`,
+    subtitle: `CHAVE SELETORA METÁLICO KNOB CURTO - 2 POS. FIXAS 90G - 1NA`,
+  },
+
+  {
+    title: `04.007.0051`,
+    subtitle: `CHAVE SELETORA METÁLICO KNOB CURTO - 3 POS. FIXAS 45G - 2NA`,
+  },
+
+  {
+    title: `04.007.0015`,
+    subtitle: `BOTÃO PULSADOR FACEADO 22MM METÁLICO - VERDE - 1NA`,
+  },
+
+  {
+    title: `- - -`,
+    subtitle: `BOTÃO PULSADOR FACEADO 22MM METÁLICO - VERMELHO - 1NA`,
+  },
+
+  {
+    title: `04.007.0054`,
+    subtitle: `BOTÃO PULSADOR FACEADO 22MM METÁLICO - VERMELHO - 1NF`,
+  },
+
+  {
+    title: `04_007_0013`,
+    subtitle: `PULSADOR ILUMINADO 22MM METÁLICO - AMARELO 24V - 1NA+1NF`,
+  },
+
+  {
+    title: `04.007.0001`,
+    subtitle: `BOTÃO EMERGÊNCIA C/TRAVA 22MM METÁLICO - VERMELHO - 1NF`,
+  },
+
+  {
+    title: `04.007.0068`,
+    subtitle: `SINALEIRO LED 22MM - 220VCA - BRANCO`,
+  },
+
+  {
+    title: `- - -`,
+    subtitle: `SINALEIRO LED 22MM - 24VCC/VCA - AZUL`,
+  },
+
+  {
+    title: `04.014.0001`,
+    subtitle: `TRANSFORMADOR DE ALTA TENSÃO PARA IGNIÇÃO - IGNITOR`,
+  },
+
+  {
+    title: `04.014.0002`,
+    subtitle: `PROGRAMADOR DE CHAMA`,
+  },
+
+  {
+    title: `04.001.0004`,
+    subtitle: `SENSOR DE CHAMA PORCELANA+CANTAL (SELCON - SEL-HT-C3-011)`,
+  },
+
+  {
+    title: `04.017.0034`,
+    subtitle: `AMPERIMETRO - QUADR. - RENZ (FM72 A15/5 ACA) INDIRETO`,
+  },
+
+  {
+    title: `- - -`,
+    subtitle: `AMPERÍMETRO DIGITAL 22MM VERDE 50 A 220VAC 0 A 100A`,
+  },
+
+  {
+    title: `04.014.0031`,
+    subtitle: `FONTE UV - TRANSFORMADOR PARA LÂMPADA UV – 5,4KVA – 220VAC PRIMÁRIO 200/220/230 - SECUNDÁRIO: 600V - KW:3 | KVA: 5,4 ISOLAÇÃO: 0,6KV | CLASSE: F (* Necessita de 3 indutores TA-2387)`,
+  },
+
+  {
+    title: `- - -`,
+    subtitle: `INDUTOR PARA LÂMPADA UV – INDUTÂNCIA: 50mH – In:8A – KVAr: 1,2 – ISOLAÇÃO: 0,6KV – CLASSE: B`,
+  },
+
+  {
+    title: `04.017.0194`,
+    subtitle: `CONVERSOR/ VARIADOR DE VELOC. - PWM - HL07 - 24V - 4,5A - HOLOTRON (HL07)`,
+  },
+];
