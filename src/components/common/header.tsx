d30f30fc31ad15eaa7ba994f4f69cafb634b2d0e
gudiiiniz <gustavodiniz.ghm@gmail.com>
2025-06-11 16:57:53 -0300

    refac: add header icons

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -1,5 +1,14 @@
 import Image from "next/image";
-import LgCombobox from "./lg-combobox"
+import LgCombobox from "./lg-combobox";
+import {
+  ChevronDown,
+  Facebook,
+  Instagram,
+  Linkedin,
+  MessageCircle,
+  Youtube,
+} from "lucide-react";
+import Link from "next/link";
 
 export default function Header() {
   return (
@@ -7,11 +16,77 @@ export default function Header() {
       <div className="min-w-screen">
         <div className="w-8/12 h-[150px] mx-auto flex flex-row justify-between items-center">
           <LgCombobox />
-          <div className="w-[276px] h-[98.25px]">
-            <Image src={"/logo.png"} alt={"logo.png"} width={276} height={98.25} />
-          </div>
-          <div className="w-[91.99px] h-[19.23]">
-            <Image src={"/bndes-logo.png"} alt={"bnds-logo"} width={91.99} height={19.23} />
+          <Link href={"/"}>
+            <Image
+              src={"/logo.png"}
+              alt={"logo.png"}
+              width={276}
+              height={98.25}
+              className="cursor-pointer"
+            />
+          </Link>
+
+          <div className="w-[200px] h-[100px] flex flex-col justify-center gap-4">
+            <a
+              href="https://www.bndes.gov.br/wps/portal/site/home"
+              target="_blank"
+            >
+              <Image
+                src={"/bndes-logo.png"}
+                alt={"bnds-logo"}
+                width={91.99}
+                height={19.23}
+                className="mx-auto cursor-pointer"
+              />
+            </a>
+            <div className="flex flex-row justify-center gap-4">
+              <a href="https://www.facebook.com/deccserigrafia" target="_blank">
+                <Facebook
+                  size={30}
+                  className="text-[#0E0458] cursor-pointer transition-transform duration-300 hover:scale-110"
+                />
+              </a>
+
+              <a
+                href="https://www.instagram.com/deccserigrafia/"
+                target="_blank"
+              >
+                <Instagram
+                  size={30}
+                  className="text-[#0E0458] cursor-pointer transition-transform duration-300 hover:scale-110"
+                />
+              </a>
+
+              <a
+                href="https://www.linkedin.com/company/deccoreserigrafia/"
+                target="_blank"
+              >
+                <Linkedin
+                  size={30}
+                  className="text-[#0E0458] cursor-pointer transition-transform duration-300 hover:scale-110"
+                />
+              </a>
+
+              <a
+                href="https://www.youtube.com/@deccoreserigrafia6780"
+                target="_blank"
+              >
+                <Youtube
+                  size={30}
+                  className="text-[#0E0458] cursor-pointer transition-transform duration-300 hover:scale-110"
+                />
+              </a>
+
+              <a
+                href="https://web.whatsapp.com/send?phone=+5511999400004"
+                target="_blank"
+              >
+                <MessageCircle
+                  size={30}
+                  className="text-[#0E0458] cursor-pointer transition-transform duration-300 hover:scale-110"
+                />
+              </a>
+            </div>
           </div>
         </div>
       </div>
